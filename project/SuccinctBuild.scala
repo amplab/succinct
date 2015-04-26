7bd953af5c1144d69d5c96e7a72e8baa7c12f152
Zongheng Yang <zongheng.y@gmail.com>
2015-04-25 21:26:58 -0400

    Add sbt build infra.

---
+++ project/SuccinctBuild.scala
@@ -0,0 +1,58 @@
+import sbt._
+import Keys._
+
+import sbtassembly.Plugin._
+import AssemblyKeys._
+
+object SuccinctBuild extends Build {
+
+  lazy val root = project.in(file("."))
+    .aggregate(core, spark)
+    .settings(assemblySettings: _*)
+    .settings(commonSettings: _*)
+
+  lazy val core = project.in(file("core"))
+    .settings(assemblySettings: _*)
+    .settings(commonSettings: _*)
+    .settings(name := "succinct-core")
+
+  lazy val spark = project.in(file("spark"))
+    .settings(assemblySettings: _*)
+    .settings(commonSettings: _*)
+    .settings(name := "succinct-spark")
+    .dependsOn(core)
+
+  lazy val commonSettings = Seq(
+    name := "succinct",
+    version := "0.1.0-SNAPSHOT",
+    organization := "edu.berkeley.cs",
+    scalaVersion := "2.10.4",
+    parallelExecution in Test := false,
+
+    libraryDependencies ++= Seq(
+      "org.scalatest" %% "scalatest" % "2.2.1" % "test"
+    ),
+
+    resolvers ++= Seq(
+     "Local Maven Repository" at Path.userHome.asFile.toURI.toURL + ".m2/repository",
+      "Typesafe" at "http://repo.typesafe.com/typesafe/releases",
+      "Spray" at "http://repo.spray.cc"
+    ),
+
+    test in assembly := {},
+
+    mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+      {
+        case PathList("javax", "servlet", xs @ _*)           => MergeStrategy.first
+        case PathList(ps @ _*) if ps.last endsWith ".html"   => MergeStrategy.first
+        case "application.conf"                              => MergeStrategy.concat
+        case "reference.conf"                                => MergeStrategy.concat
+        case "log4j.properties"                              => MergeStrategy.discard
+        case m if m.toLowerCase.endsWith("manifest.mf")      => MergeStrategy.discard
+        case m if m.toLowerCase.matches("meta-inf.*\\.sf$")  => MergeStrategy.discard
+        case _ => MergeStrategy.first
+      }
+    }
+  )
+
+}
