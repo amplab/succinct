4bc338545e4bdb4316fcb2c33bce576596339dce
Anurag Khandelwal <anuragk@berkeley.edu>
2015-07-04 18:30:11 -0700

    Use old API

--- spark/src/main/scala/edu/berkeley/cs/succinct/storage/TachyonStorageManager.scala
+++ spark/src/main/scala/edu/berkeley/cs/succinct/storage/TachyonStorageManager.scala
@@ -2,15 +2,14 @@ package edu.berkeley.cs.succinct.storage
 
 import edu.berkeley.cs.succinct.SuccinctIndexedFile
 import edu.berkeley.cs.succinct.buffers.SuccinctIndexedFileBuffer
-import tachyon.TachyonURI
 import tachyon.client.TachyonFS
 
 private[succinct] object TachyonStorageManager {
 
   def loadFromTachyon(path: String): SuccinctIndexedFile = {
-    val pathURI = new TachyonURI(path)
-    val client = TachyonFS.get(pathURI)
-    val file = client.getFile(pathURI)
+    // val pathURI = new TachyonURI(path)
+    val client = TachyonFS.get(path)
+    val file = client.getFile(path)
     var buf = file.readByteBuffer(0)
     if (buf == null) {
       file.recache()
