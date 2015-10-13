60da1dce9dddca32e0c664e3e012555581ef77af
Anurag Khandelwal <anuragk@berkeley.edu>
2015-10-13 14:15:25 -0700

    Added KV and Table benchmarks

---
+++ spark/src/main/scala/edu/berkeley/cs/succinct/examples/KVBench.scala
@@ -0,0 +1,136 @@
+package edu.berkeley.cs.succinct.examples
+
+import java.io.FileWriter
+
+import edu.berkeley.cs.succinct.kv.SuccinctKVRDD
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+
+import scala.reflect.ClassTag
+import scala.util.Random
+
+object KVBench {
+
+  // Constants
+  val WARMUP_COUNT: Int = 100
+  val MEASURE_COUNT: Int = 1000
+
+  // Queries
+  var keys: Array[java.lang.Long] = _
+  var keysWarmup: Array[java.lang.Long] = _
+  var keysMeasure: Array[java.lang.Long] = _
+
+  // Output path
+  var outPath: String = _
+
+  def sampleArr[T: ClassTag](input: Array[T], sampleSize: Int): Array[T] = {
+    Array.fill(sampleSize)(input(Random.nextInt(input.length)))
+  }
+
+  def get(rdd: RDD[(java.lang.Long, Array[Byte])], key: java.lang.Long): Array[Byte] = {
+    val res = rdd.filter(kv => kv._1 == key).collect()
+    if (res.size == 0) {
+      throw new ArrayIndexOutOfBoundsException("Invalid key = " + key)
+    }
+    if (res.size > 0) {
+      throw new IllegalArgumentException("Got multiple values for key = " + key)
+    }
+    res(0)._2
+  }
+
+  def benchSparkRDD(rdd: RDD[(java.lang.Long, Array[Byte])]): Unit = {
+    val storageLevel = rdd.getStorageLevel match {
+      case StorageLevel.DISK_ONLY => "disk"
+      case StorageLevel.MEMORY_ONLY => "mem"
+      case _ => "undf"
+    }
+
+    println(s"Benchmarking Spark RDD $storageLevel get...")
+
+    // Warmup
+    keysWarmup.foreach(k => {
+      val length = get(rdd, k).length
+      println(s"$k\t$length")
+    })
+
+    // Measure
+    val outGet = new FileWriter(outPath + "/spark-" + storageLevel + "-get")
+    keysMeasure.foreach(k => {
+      val startTime = System.currentTimeMillis()
+      val length = get(rdd, k).length
+      val endTime = System.currentTimeMillis()
+      val totTime = endTime - startTime
+      outGet.write(s"$k\t$length\t$totTime\n")
+    })
+    outGet.close()
+  }
+
+  def benchSuccinctRDD(rdd: SuccinctKVRDD[java.lang.Long]): Unit = {
+    println("Benchmarking Succinct RDD get...")
+
+    println("Benchmarking Succinct RDD random access...")
+    keysWarmup.foreach(k => {
+      val length = rdd.get(k).length
+      println(s"$k\t$length")
+    })
+
+    // Measure
+    val outGet = new FileWriter(outPath + "/succinct-get")
+    keysMeasure.foreach(k => {
+      val startTime = System.currentTimeMillis()
+      val length = rdd.get(k).length
+      val endTime = System.currentTimeMillis()
+      val totTime = endTime - startTime
+      outGet.write(s"$k\t$length\t$totTime\n")
+    })
+    outGet.close()
+
+  }
+
+  def main(args: Array[String]) {
+    if (args.length < 5) {
+      System.err.println("Usage: KVBench <raw-data> <succinct-data> <partitions> <output-path>")
+      System.exit(1)
+    }
+
+    val dataPath = args(0)
+    val succinctDataPath = args(1)
+    val partitions = args(2).toInt
+    outPath = args(3)
+
+    val sparkConf = new SparkConf().setAppName("KVBench")
+    val ctx = new SparkContext(sparkConf)
+
+    val kvRDD = ctx.textFile("/conviva-sql-raw")
+      .zipWithIndex
+      .map(t => (t._2.asInstanceOf[java.lang.Long], t._1.getBytes))
+      .repartition(32)
+
+    val kvRDDDisk = kvRDD.persist(StorageLevel.DISK_ONLY)
+    val count = kvRDDDisk.count()
+    println("Number of entries = " + kvRDDDisk.count())
+
+    keys = Random.shuffle((0 to 9999)
+      .map(i => (Random.nextLong() % count).asInstanceOf[java.lang.Long]))
+      .toArray
+
+    // Create queries
+    keysWarmup = sampleArr(keys, WARMUP_COUNT)
+    keysMeasure = sampleArr(keys, MEASURE_COUNT)
+
+    benchSparkRDD(kvRDDDisk)
+    kvRDDDisk.unpersist(true)
+
+    val kvRDDMem = kvRDD.persist(StorageLevel.MEMORY_ONLY)
+    println("Number of entries = " + kvRDDMem.count())
+
+    benchSparkRDD(kvRDDMem)
+    kvRDDMem.unpersist(true)
+
+    val kvRDDSuccinct = SuccinctKVRDD[java.lang.Long](ctx, succinctDataPath, StorageLevel.MEMORY_ONLY).cache()
+    println("Number of entries = " + kvRDDSuccinct.count())
+
+    benchSuccinctRDD(kvRDDSuccinct)
+  }
+}
---
+++ spark/src/main/scala/edu/berkeley/cs/succinct/examples/TableBench.scala
@@ -0,0 +1,84 @@
+package edu.berkeley.cs.succinct.examples
+
+import java.io.FileWriter
+
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql.{SQLContext, DataFrame}
+
+import scala.io.Source
+import scala.reflect.ClassTag
+import scala.util.Random
+
+object TableBench {
+
+  // Queries
+  var queries: Array[String] = _
+  var warmupQueries: Array[String] = _
+  var measureQueries: Array[String] = _
+
+  // Constants
+  val WARMUP_COUNT: Int = 10
+  val MEASURE_COUNT: Int = 1000
+
+  // Output path
+  var outPath: String = _
+
+  def sampleArr[T: ClassTag](input: Array[T], sampleSize: Int): Array[T] = {
+    Array.fill(sampleSize)(input(Random.nextInt(input.length)))
+  }
+
+  def filterQuery(query: String, df: DataFrame): DataFrame = {
+    df.filter(query)
+  }
+
+  def benchDF(df: DataFrame, dfType: String) = {
+    println(s"Benchmarking DataFrame $dfType...")
+
+    // Warmup
+    warmupQueries.foreach(k => {
+      val length = filterQuery(k, df).count()
+      println(s"$k\t$length")
+    })
+
+    // Measure
+    val outGet = new FileWriter(outPath + "/df-" + dfType)
+    measureQueries.foreach(k => {
+      val startTime = System.currentTimeMillis()
+      val length = filterQuery(k, df).count()
+      val endTime = System.currentTimeMillis()
+      val totTime = endTime - startTime
+      outGet.write(s"$k\t$length\t$totTime\n")
+    })
+    outGet.close()
+  }
+
+  def main(args: Array[String]) {
+    if (args.length < 5) {
+      System.err.println("Usage: TableBench <parquet-data> <succinct-data> <query-path> <output-path>")
+      System.exit(1)
+    }
+
+    val parquetDataPath = args(0)
+    val succinctDataPath = args(1)
+    val queryPath = args(2)
+    outPath = args(3)
+
+    queries = Source.fromFile(queryPath)
+      .getLines()
+      .map(line => line.split('\t'))
+      .map(lineArr => "col" + lineArr(0) + "=" + lineArr(1))
+      .toArray
+
+    warmupQueries = sampleArr(queries, WARMUP_COUNT)
+    measureQueries = sampleArr(queries, MEASURE_COUNT)
+
+    val sqlCtx = new SQLContext(new SparkContext(new SparkConf().setAppName("TableBench")))
+
+    val parquetDF = sqlCtx.read.parquet(parquetDataPath)
+    benchDF(parquetDF, "parquet")
+
+    val succinctDF = sqlCtx.read.format("edu.berkeley.cs.succinct.sql").load(succinctDataPath)
+    benchDF(succinctDF, "succinct")
+  }
+
+}
