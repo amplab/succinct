52d30430c830a45ee05fd840077b435d5a38f27e
Anurag Khandelwal <anuragk@berkeley.edu>
2015-11-02 15:04:29 -0800

    Added convenient implicit calls for SuccinctRDD

---
+++ spark/src/main/scala/edu/berkeley/cs/succinct/package.scala
@@ -0,0 +1,21 @@
+package edu.berkeley.cs
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+
+package object succinct {
+  implicit class SuccinctContext(sc: SparkContext) {
+    def succinctFile(filePath: String, storageLevel: StorageLevel = StorageLevel.MEMORY_ONLY): SuccinctRDD = {
+      SuccinctRDD(sc, filePath, storageLevel)
+    }
+  }
+
+  implicit class SuccinctFlatRDD(rdd: RDD[Array[Byte]]) {
+    def succinct: SuccinctRDD = {
+      SuccinctRDD(rdd)
+    }
+
+    def saveAsSuccinctFile(path: String): Unit = SuccinctRDD(rdd).save(path)
+  }
+}
