892f9cd6c6d1a9006e1c4aeac07127c81c3a11bb
Anurag Khandelwal <anuragk@berkeley.edu>
2015-11-05 10:37:05 -0800

    Updated Construct example to construct both flat and indexed files

--- core/src/main/java/edu/berkeley/cs/succinct/examples/Construct.java
+++ core/src/main/java/edu/berkeley/cs/succinct/examples/Construct.java
@@ -2,36 +2,72 @@ package edu.berkeley.cs.succinct.examples;
 
 import edu.berkeley.cs.succinct.StorageMode;
 import edu.berkeley.cs.succinct.buffers.SuccinctFileBuffer;
+import edu.berkeley.cs.succinct.buffers.SuccinctIndexedFileBuffer;
 
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Construct {
   public static void main(String[] args) throws IOException {
-    if (args.length != 2) {
-      System.err.println("Parameters: [input-path] [output-path]");
+    if (args.length < 2 || args.length > 3) {
+      System.err.println("Parameters: [input-path] [output-path] <[type]>");
       System.exit(-1);
     }
 
-    SuccinctFileBuffer succinctFileBuffer;
+    SuccinctFileBuffer succinctFileBuffer = null;
 
-    if (args[0].endsWith(".succinct")) {
-      succinctFileBuffer = new SuccinctFileBuffer(args[0], StorageMode.MEMORY_ONLY);
-    } else {
-      File file = new File(args[0]);
-      if (file.length() > 1L << 31) {
-        System.err.println("Cant handle files > 2GB");
-        System.exit(-1);
-      }
-      byte[] fileData = new byte[(int) file.length()];
-      System.out.println("File size: " + fileData.length + " bytes");
-      DataInputStream dis = new DataInputStream(new FileInputStream(file));
-      dis.readFully(fileData, 0, (int) file.length());
-
-      succinctFileBuffer = new SuccinctFileBuffer(fileData);
+    String type = "file";
+    if (args.length == 3) {
+      type = args[2];
     }
+
+    if (type == "file") {
+      if (args[0].endsWith(".succinct")) {
+        succinctFileBuffer = new SuccinctFileBuffer(args[0], StorageMode.MEMORY_ONLY);
+      } else {
+        File file = new File(args[0]);
+        if (file.length() > 1L << 31) {
+          System.err.println("Cant handle files > 2GB");
+          System.exit(-1);
+        }
+        byte[] fileData = new byte[(int) file.length()];
+        System.out.println("File size: " + fileData.length + " bytes");
+        DataInputStream dis = new DataInputStream(new FileInputStream(file));
+        dis.readFully(fileData, 0, (int) file.length());
+
+        succinctFileBuffer = new SuccinctFileBuffer(fileData);
+      }
+    } else if (type == "indexed-file") {
+      if (args[0].endsWith(".succinct")) {
+        succinctFileBuffer = new SuccinctIndexedFileBuffer(args[0], StorageMode.MEMORY_ONLY);
+      } else {
+        File file = new File(args[0]);
+        if (file.length() > 1L << 31) {
+          System.err.println("Cant handle files > 2GB");
+          System.exit(-1);
+        }
+        byte[] fileData = new byte[(int) file.length()];
+        System.out.println("File size: " + fileData.length + " bytes");
+        DataInputStream dis = new DataInputStream(new FileInputStream(file));
+        dis.readFully(fileData, 0, (int) file.length());
+
+        ArrayList<Integer> positions = new ArrayList<Integer>();
+        positions.add(0);
+        for (int i = 0; i < fileData.length; i++) {
+          if (fileData[i] == '\n') {
+            positions.add(i + 1);
+          }
+        }
+        int[] offsets = new int[positions.size()];
+
+        succinctFileBuffer = new SuccinctIndexedFileBuffer(fileData, offsets);
+      }
+    }
+
+    assert succinctFileBuffer != null;
 
     succinctFileBuffer.writeToFile(args[1]);
   }
