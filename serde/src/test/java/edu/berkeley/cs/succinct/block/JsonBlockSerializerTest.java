bb2e97b01c471ea7c4f1a69bdc431d4a814ebd10
Anurag Khandelwal <anuragk@berkeley.edu>
2015-11-08 17:42:56 -0800

    Updated tests

--- serde/src/test/java/edu/berkeley/cs/succinct/block/JsonBlockSerializerTest.java
+++ serde/src/test/java/edu/berkeley/cs/succinct/block/JsonBlockSerializerTest.java
@@ -12,7 +12,7 @@ public class JsonBlockSerializerTest extends TestCase {
   public void testSerialize() throws Exception {
 
     String jsonString1 = "{\"name\":\"abc\", \"age\":18}";
-    String jsonString2 = "{\"name\": { \"first\" : \"abc\", \"last\" : \"def\" }, \"age\":18}";
+    String jsonString2 = "{\"name\":{\"first\":\"abc\", \"last\":\"def\"}, \"age\":16.5}";
     String[] jsonData = new String[] {jsonString1, jsonString2};
 
     byte[] delimiters = new byte[120];
@@ -45,7 +45,7 @@ public class JsonBlockSerializerTest extends TestCase {
 
     // Check serialized data
     byte[] expectedSerializedData = new byte[]{-120, 'a', 'b', 'c', -120, -119, '1', '8', -119,
-      -118, 'a', 'b', 'c', -118, -117, 'd', 'e', 'f', -117, -119, '1', '8', -119};
+      -118, 'a', 'b', 'c', -118, -117, 'd', 'e', 'f', -117, -119, '1', '6', '.' , '5', -119};
     assertTrue(Arrays.equals(expectedSerializedData, serializedData.getData()));
 
   }
