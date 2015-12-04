73c8f6064d0c8a8a74bf15a778e51b18f52c515a
Anurag Khandelwal <anuragk@berkeley.edu>
2015-12-04 01:59:20 -0800

    Added SuccinctConstants

---
+++ core/src/main/java/edu/berkeley/cs/succinct/util/SuccinctConstants.java
@@ -0,0 +1,17 @@
+package edu.berkeley.cs.succinct.util;
+
+public class SuccinctConstants {
+
+  // Type sizes
+  public static final int BYTE_SIZE_BYTES = 1;
+  public static final int CHAR_SIZE_BYTES = 2;
+  public static final int SHORT_SIZE_BTYES = 2;
+  public static final int INT_SIZE_BYTES = 4;
+  public static final int LONG_SIZE_BYTES = 8;
+
+  // Default Sampling Rates
+  public static final int DEFAULT_SA_SAMPLING_RATE = 32;
+  public static final int DEFAULT_ISA_SAMPLING_RATE = 32;
+  public static final int DEFAULT_NPA_SAMPLING_RATE = 128;
+
+}
